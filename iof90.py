8e53927e89683fcf4608a038e1af9006a5683263
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2020-12-10 13:14:18 -0800

    update

--- iof90.py
+++ iof90.py
@@ -2,27 +2,31 @@
 import numpy as np
 from scipy.io import FortranFile
 
+
 def writef90map(fname,d1):
-  """
-  Save data to a fortran file
-  """
-  f = FortranFile(fname,'w')
-  #f.write_record(np.int16(dim))
-  f.write_record(np.float64(d1))
-  f.close()
+    """
+    Save data to a fortran file
+    """
+    f = FortranFile(fname,'w')
+    #f.write_record(np.int16(dim))
+    f.write_record(np.float64(d1))
+    f.close()
+
 
 def readf90map(fname):
-  f = FortranFile(fname,'r')
-  dat = f.read_ints(dtype=np.float64)
-  f.close()
-  return dat
+    f = FortranFile(fname,'r')
+    dat = f.read_ints(dtype=np.float64)
+    f.close()
+    return dat
+
 
 def readfile(fname,dt=np.float64,hdrnum=0):
-  f = open(fname,'r')
-  if hdrnum>0: 
-    hdr = np.fromfile(f,dtype=np.int32,count=hdrnum)
-    print(hdr)
-  dat = np.fromfile(f,dtype=dt)
-  f.close()
-  return dat
+    f = open(fname,'r')
+    if hdrnum>0: 
+        hdr = np.fromfile(f,dtype=np.int32,count=hdrnum)
+        print(hdr)
+    dat = np.fromfile(f,dtype=dt)
+    f.close()
+    return dat
+
 
--- myutils.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-#//// text adds to a file ////#
-def add(text,f,ini=False,opt=''):
-  if ini:     os.system('echo '+opt+' "'+text+'" > '+f)
-  if not ini: os.system('echo '+opt+' "'+text+'" >> '+f)
-
